3937396c307e82f3e2e7e553c20d3e4411613df1
Supermagzzz <30972311+Supermagzzz@users.noreply.github.com>
2019-03-19 07:55:20 +0300

    teleport

---
+++ main/Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject teleportPoint;
+    public GameObject teleport;
+    
+	void Start ()
+    {
+        /* Расстановка телепортов */
+		for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                GameObject tmpTeleport = Instantiate(teleportPoint, new Vector3(i, 0, j), Quaternion.identity);
+                tmpTeleport.transform.parent = teleport.transform;
+            }
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
