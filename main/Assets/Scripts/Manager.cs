d40ef36cf80007c16b29ab9ad2e7fa08557e17ad
Supermagzzz <30972311+Supermagzzz@users.noreply.github.com>
2019-03-19 10:21:42 +0300

    final commit 1 day

--- main/Assets/Scripts/Manager.cs
+++ main/Assets/Scripts/Manager.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour {
-
-    public GameObject teleportPoint;
-    public GameObject teleport;
     
 	void Start ()
     {
-        /* Расстановка телепортов */
-		for(int i = 0; i < 10; i++)
-        {
-            for(int j = 0; j < 10; j++)
-            {
-                //GameObject tmpTeleport = Instantiate(teleportPoint);
-                //tmpTeleport.transform.position = new Vector3(i, 0, j);
-
-                //tmpTeleport.transform.parent = teleport.transform;
-            }
-        }
+        
 	}
 	
-	void Update () {
-		
+	void Update ()
+    {
+	    
 	}
 }
